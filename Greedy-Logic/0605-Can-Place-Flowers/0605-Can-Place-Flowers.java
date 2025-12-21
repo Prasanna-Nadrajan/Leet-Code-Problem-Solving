b901e8e3cbebe7f27405a2414915a6bc221ab7b6
Prasanna Nadrajan <prasannanadrajan@gmail.com>
2025-12-21 21:39:14 +0530

    Implement canPlaceFlowers method in Solution class

---
+++ Greedy-Logic/0605-Can-Place-Flowers/0605-Can-Place-Flowers.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if(n==0){
+            return true;
+        }
+        int f=flowerbed.length;
+        for(int i=0;i<f;i++){
+            if(flowerbed[i]==0 && (i==0 || flowerbed[i-1]==0) && (i==f-1 || flowerbed[i+1]==0)){
+                n--;
+                flowerbed[i]=1;
+                if(n==0){
+                    return true;
+                }
+            }
+        }
+        return false;
+        // return true;
+    }
+}
